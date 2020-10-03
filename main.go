9dd285560f120bcb4f378bbb1102eb22efd5d9c9
Song-Hua, Yang <songhua.develop@gmail.com>
2020-10-03 22:12:13 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,22 +2,48 @@ package gosms
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 )
 
 var config Config
+var Config2 Config
 
-func Initialization(input Config) {
+// 初始化
+func Init(input Config) {
 	config = input
-
 }
 
-func SendText(mobile, message string) {
+// 發送文字訊息
+func SendText(mobile, message string) (bool, error) {
 
-	aaa := fmt.Sprintf("https://api.twsms.com/json/sms_send.php?username=%s&password=%s", config.Account, config.Password)
-	ssw := fmt.Sprintf("%s&mobile=%s&message=%s", aaa, mobile, message)
+	// 獲取完整路徑
+	fullURL := formatFullURL(mobile, message)
 
-	log.Print(ssw)
+	resp, err := http.Get(fullURL)
+
+	if err != nil {
+		return false, nil
+	}
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return false, nil
+	}
+
+	log.Print(string(bytes))
+
+	return true, nil
+}
+
+func formatFullURL(mobile, message string) string {
+	baseURL := "https://api.twsms.com/json/sms_send.php"
+	fullURL := fmt.Sprintf("%s?username=%s&password=%s", baseURL, config.Account, config.Password)
+	fullURL  = fmt.Sprintf("%s&mobile=%s&message=%s", fullURL, mobile, message)
+
+	return fullURL
 }
 
 type Config struct {
