4e7cc7483bb7410a8b20a144734c4c082415df29
Song-Hua, Yang <songhua.develop@gmail.com>
2020-10-03 21:58:48 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,10 +2,24 @@ package gosms
 
 import "fmt"
 
-var config Config
+var config2 Config
 
 func OOOO() {
 	fmt.Print(9999)
+}
+
+func Initialization(config Config) {
+	config2 = config
+
+
+}
+
+func SendText(mobile, message string) {
+
+	aaa := fmt.Sprintf("https://api.twsms.com/json/sms_send.php?username=%s&password=%s")
+	ssw := fmt.Sprintf("%s&mobile=%s&message=%s", aaa, mobile, message)
+
+	fmt.Sprintf(ssw)
 }
 
 type Config struct {
